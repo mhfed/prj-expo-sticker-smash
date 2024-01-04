065980e73420bb28d99dcf5953d0ad60e4af55bf
hieu.nguyen <hieu.nguyen@novus-fintech.com>
2024-01-04 14:02:38 +0700

    feat(all): add new images and components

---
+++ components/EmojiSticker.js
@@ -0,0 +1,58 @@
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+export default function EmojiSticker({ imageSize, stickerSource }) {
+  const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    });
+
+  const drag = Gesture.Pan().onChange((e) => {
+    translateX.value += e.changeX;
+    translateY.value += e.changeY;
+  });
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      height: withSpring(scaleImage.value),
+      width: withSpring(scaleImage.value),
+    };
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
+  return (
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode='contain'
+            /* @info Modify the style prop on the AnimatedImage to pass the imageStyle. */ style={[
+              imageStyle,
+              { width: imageSize, height: imageSize },
+            ]} /* @end */
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
+  );
+}
